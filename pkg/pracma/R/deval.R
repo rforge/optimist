d6b28ebd50ac2d37c65b8490c2f5188dfe320851
hwborchers <hwborchers@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-05-27 10:35:56 +0000

    pracma 0.4-7 deval, findintervals
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimist@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pracma/R/deval.R
+++ pkg/pracma/R/deval.R
@@ -1,9 +1,16 @@
+##
+##  d e v a l . R
+##
+
+
 deval <- function(x, y, xp, idx = NULL) {
     stopifnot(is.vector(x, mode = "numeric"), is.numeric(y),
               is.vector(xp, mode= "numeric"))
     if (is.vector(y)) y <- as.matrix(y)
     if (length(x) != nrow(y))
         stop("Length of 'x' must be equal to the number of rows in 'y'.")
+    if (is.unsorted(x))
+        stop("Argument vector 'x' must be sorted.")
     if (is.null(idx)) idx <- 1:ncol(y)
     if (! all(idx %in% 1:ncol(y)))
         stop("Indices 'idx' must be between 1 and no. of columns of 'y'.")
@@ -15,10 +22,17 @@ deval <- function(x, y, xp, idx = NULL) {
 
     for (i in 1:flen) {
         fi <- fint[i]
-        if (fi == 0 || fi == length(x)) next
-
-        yp[i, ] <- y[fi, idx] + 
-                   (xp[i] - x[fi])/(x[fi+1] - x[fi]) * (y[fi+1, idx] - y[fi, idx])
+        if (fi == 0) next
+        if (fi < length(x)) {
+            yp[i, ] <- y[fi, idx] + 
+                       (xp[i] - x[fi])/(x[fi+1] - x[fi]) * (y[fi+1, idx] - y[fi, idx])
+        } else {
+            if (xp[i] > x[length(x)]) {
+                next
+            } else {
+                yp[i, ] <- y[fi, idx]
+            }
+        }
     }
 
     if (flen == 1) yp <- drop(yp)
@@ -43,17 +57,18 @@ deeve <- function(x, y, yv = 0, idx = NULL){
         return(NA)
 
     # findInterval() needs nondecreasingly sorted vector
-    i1 <- which(yv == y)
-    y1 <- (y - yv)[1:(length(y)-1)]
-    y2 <- (y - yv)[2:length(y)]
-    i2 <- which(y1 * y2 < 0)
-    fint <- sort(c(i1, i2))
+    fint <- findintervals(yv, y)
     flen <- length(fint)
+    if (flen == 0) return(c())
 
     x0 <- numeric(flen)
     for (i in 1:flen) {
         fi <- fint[i]
-        x0[i] <- (yv - y[fi]) / (y[fi+1] - y[fi]) * (x[fi+1] - x[fi]) + x[fi]
+        if (fi < length(y)) {
+            x0[i] <- (yv - y[fi]) / (y[fi+1] - y[fi]) * (x[fi+1] - x[fi]) + x[fi]
+        } else {
+            x0[i] <- x[fi]
+        }
     }
 
     return(x0)
