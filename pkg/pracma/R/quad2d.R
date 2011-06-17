9eaa77feca5a0c8a083506cd93870ad4d97938bd
hwborchers <hwborchers@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-17 18:40:38 +0000

    pracma 0.5-6 quadv(), dblquad()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimist@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pracma/R/quad2d.R
+++ pkg/pracma/R/quad2d.R
@@ -1,3 +1,8 @@
+##
+##  q u a d 2 d . R  and  q u a d v .R
+##
+
+
 quad2d <- function(f, xa, xb, ya, yb, n = 25, ...) {
     stopifnot(is.numeric(xa), length(xa) == 1, is.numeric(ya), length(ya) == 1,
               is.numeric(xb), length(xb) == 1, is.numeric(yb), length(yb) == 1)
@@ -18,5 +23,23 @@ quad2d <- function(f, xa, xb, ya, yb, n = 25, ...) {
     Z <- f(mgrid$X, mgrid$Y)
 
     Q <- wx %*% Z %*% as.matrix(wy)
+    return(Q)
+}
+
+
+quadv <- function(f, a, b, n = 25, ...) {
+    stopifnot(is.numeric(a), length(a) == 1, is.numeric(b), length(b) == 1)
+
+    fun <- match.fun(f)
+    f <- function(x) fun(x, ...)
+
+    # Get Gauss_legendre nodes and weights
+    cc <- gaussLegendre(n, a, b)
+    x <- cc$x
+    w <- cc$w
+
+    Q <- 0
+    for (i in 1:n) Q <- Q + w[i] * f(x[i])
+
     return(Q)
 }
