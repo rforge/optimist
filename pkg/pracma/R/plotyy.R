6aeb50e9a365c6696325d656d01785ca77eb2df0
hwborchers <hwborchers@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-12 17:42:40 +0000

    corrections to plotyy
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimist@122 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pracma/R/plotyy.R
+++ pkg/pracma/R/plotyy.R
@@ -1,4 +1,4 @@
-plotyy <- function(x, y1, y2, grid = TRUE,
+plotyy <- function(x, y1, y2, grid = TRUE, box.col = "grey",
                    type = "l", lwd = 1, lty = 1,
                    col.y1 = "navy", col.y2 = "maroon", ...) {
     stopifnot(is.numeric(x), is.numeric(y1), is.numeric(y2))
@@ -12,15 +12,21 @@ plotyy <- function(x, y1, y2, grid = TRUE,
     y2pretty <- y1l + (y2range - y2l)/(y2u - y2l) * (y1u - y1l)
 
     opar <- par(mar = c(5.1, 4.1, 4.1, 3.1))
-    plot(range(x), range(c(y1, y2p)), type = "n", yaxt = "n",
+    plot(range(x), range(c(y1, y2p)), type = "n", yaxt = "n", bty = "n", 
          xlab = "x", ylab = "y", ...)
-    box(col = "grey")
-    axis(side = 2, at=NULL, col = col.y1)
-    axis(side = 4, at = y2pretty, labels = y2range, col = col.y2)
+    box(col = box.col)
+    # axis(side = 2, at = NULL, labels = "n", col = col.y1)
+    # axis(side = 4, at = y2pretty, labels = y2range, col = col.y2)
+
+    mx <- axis(side = 2, labels = FALSE, col = col.y1)
+    my <- axis(side = 4, labels = FALSE, col = col.y2)
+    my <- y2l + (my - y1l)/(y1u - y1l) * (y2u - y2l)
+    mtext(mx, side = 2, line = 1, at = mx, col = col.y1)
+    mtext(my, side = 4, line = 1, at = mx, col = col.y2)
 
     if (grid) grid()
-    points(x, y1, type = type, col = col.y1, lwd = lwd, lty = lty)
-    lines(x, y2p, type = type, col = col.y2, lwd = lwd, lty = lty)
+    points(x, y1,  type = type, col = col.y1, lwd = lwd, lty = lty)
+    points(x, y2p, type = type, col = col.y2, lwd = lwd, lty = lty)
     par(opar)
 
     invisible()
