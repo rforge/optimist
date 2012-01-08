63b152c7a7321be92c5bf117706ef40e1e5144e2
hwborchers <hwborchers@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-08 13:25:47 +0000

    first versions 'adagio' and 'convoi'.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimist@136 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/convoi/R/maxsub.R
@@ -0,0 +1,54 @@
+maxsub_r <-
+function(x, inds = FALSE) {
+    if (!is.numeric(x))
+        stop("Argument 'x' must be a numeric vector.")
+    n <- length(x)
+
+    if (!inds) {
+        m1 <- m2 <- 0.0
+        for (i in 1:n) {
+            m2 <- max(m2 + x[i], 0.0)
+            m1 <- max(m1, m2)
+        }
+        return(m1)
+
+    } else {
+        m1 <- m2 <- 0
+        p1 <- p2 <- 0
+        q1 <- q2 <- 1
+
+        for (i in 1:n) {
+            if (m2 > -x[i]) {
+                m2 <- m2 + x[i]
+                q2 <- i
+                if (m2 > m1) {
+                    m1 <- m2
+                    p1 <- q1; p2 <- q2
+                }
+            } else {
+                m2 <- 0
+                q1 <- q2 <- i+1
+            }
+        }
+
+        return(list(sum = m1, inds = c(p1, p2)))
+    }
+}
+
+
+maxsub <- function(x, inds = TRUE) {
+    if (!is.numeric(x))
+        stop("Argument 'x' must be a numeric vector.")
+    n <- length(x)
+    i1 <- 0; i2 <- 0
+    s <- 0.0
+
+    S <- .Fortran("maxsubf", x = as.numeric(x), n = as.integer(n),
+                             s = as.numeric(s),
+                             i1 = as.integer(i1), i2 = as.integer(i2),
+                             PACKAGE = 'myRtests')
+    if (inds)
+        return(list(sum = S$s, inds = c(S$i1, S$i2)))
+    else
+        return(S$s)
+}
---
+++ pkg/convoi/src/maxsubf.f
@@ -0,0 +1,28 @@
+C Maximal contiguous subset problem
+      subroutine maxsubf(x, n, s, i1, i2)
+      integer n, i1, i2, j1, j2
+      double precision x(n)
+      double precision s, ss
+
+      ss = 0
+      j1 = 1
+      j2 = 1
+
+      do 23000 i = 1,n 
+      if (ss .gt. -x(i)) then
+        ss = ss + x(i)
+        j2 = i
+        if (ss .gt. s) then
+          s  = ss
+          i1 = j1
+          i2 = j2
+        endif
+      else
+        ss = 0
+        j1 = i+1
+        j2 = i+1
+      endif
+23000 continue
+
+      return
+      end
