de2c5804df76955400e9c1c4ee4899a6e8124fa0
hwborchers <hwborchers@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-02 20:34:05 +0000

    Package 'nloptwrap' 0.3-3 with MMA and variable-metric minimization methods.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimist@282 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nloptwrap/R/global.R
@@ -0,0 +1,117 @@
+##
+##  s t o g o . R
+##
+
+
+#-- Does not work: maybe C++ support missing? ---------------------- StoGo ---
+stogo <-
+function(x0, fn, gr = NULL, lower = NULL, upper = NULL,
+            nl.info = FALSE, control = list(), ...)
+{
+    opts <- nl.opts(control)
+    opts["algorithm"] <- "NLOPT_GD_STOGO"  # NLOPT_GD_STOGO_RAND
+
+    fun <- match.fun(fn)
+    fn  <- function(x) fun(x, ...)
+
+    if (is.null(gr)) {
+        gr <- function(x) nl.grad(x, fn)
+    }
+
+    S0 <- nloptr(x0,
+                eval_f = fn,
+                eval_grad_f = gr,
+                lb = lower,
+                ub = upper,
+                opts = opts)
+
+    if (nl.info) print(S0)
+    S1 <- list(par = S0$solution, value = S0$objective, iter = S0$iterations,
+                convergence = S0$status, message = S0$message)
+    return(S1)
+}
+
+
+#-- Supports nonlinear constraints: quite inaccurate! -------------- ISRES ---
+isres <-
+function(x0, fn, lower, upper,
+            hin = NULL, heq = NULL,
+            nl.info = FALSE, control = list(), ...)
+{
+    opts <- nl.opts(control)
+    opts["algorithm"] <- "NLOPT_GN_ISRES"
+
+    fun <- match.fun(fn)
+    fn  <- function(x) fun(x, ...)
+
+    if (!is.null(hin)) {
+        .hin <- match.fun(hin)
+        hin <- function(x) (-1) * .hin(x)   # change  hin >= 0  to  hin <= 0 !
+    }
+
+    if (!is.null(heq)) {
+        .heq <- match.fun(heq)
+        heq <- function(x) .heq(x)
+    }
+
+    S0 <- nloptr(x0 = x0,
+                eval_f = fn,
+                lb = lower,
+                ub = upper,
+                eval_g_ineq = hin,
+                eval_g_eq = heq,
+                opts = opts)
+
+    if (nl.info) print(S0)
+    S1 <- list(par = S0$solution, value = S0$objective, iter = S0$iterations,
+                convergence = S0$status, message = S0$message)
+    return(S1)
+}
+
+
+#-- ------------------------------------------------------------------ CRS ---
+crs2lm <-
+function(x0, fn, lower, upper,
+            nl.info = FALSE, control = list(), ...)
+{
+    opts <- nl.opts(control)
+    opts["algorithm"] <- "NLOPT_GN_CRS2_LM"
+
+    fun <- match.fun(fn)
+    fn  <- function(x) fun(x, ...)
+
+    S0 <- nloptr(x0,
+                eval_f = fn,
+                lb = lower,
+                ub = upper,
+                opts = opts)
+
+    if (nl.info) print(S0)
+    S1 <- list(par = S0$solution, value = S0$objective, iter = S0$iterations,
+                convergence = S0$status, message = S0$message)
+    return(S1)
+}
+
+
+#-- Needs setting a local optimizer: how? --------------------------- MLSL ---
+mlsl <-
+function(x0, fn, lower, upper,
+            nl.info = FALSE, control = list(), ...)
+{
+    opts <- nl.opts(control)
+    opts["algorithm"] <- ""
+
+    fun <- match.fun(fn)
+    fn  <- function(x) fun(x, ...)
+
+    S0 <- nloptr(x0,
+                eval_f = fn,
+                lb = lower,
+                ub = upper,
+                opts = opts)
+
+    if (nl.info) print(S0)
+    S1 <- list(par = S0$solution, value = S0$objective, iter = S0$iterations,
+                convergence = S0$status, message = S0$message)
+    return(S1)
+}
