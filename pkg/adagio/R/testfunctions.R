9afaa81fddb1facd6f8ae1937d79493a2c68fdb9
hwborchers <hwborchers@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-14 08:56:31 +0000

    adagio with testfunctions and CMA-ES
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimist@319 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/adagio/R/testfunctions.R
+++ pkg/adagio/R/testfunctions.R
@@ -1,6 +1,25 @@
 ##
-##  t e s t f u n c t i o n s . R  Test Functions
+##  t e s t f u n c t i o n s . R
 ##
+
+
+genTestfn <- function(fname) {
+	fname <- tolower(fname)
+	tfunc <- switch(fname,
+					hald = list(fn = fnHald, gr = grHald),
+	                nesterov = list(fn = fnNesterov, gr = grNesterov),
+	                nesterov1 = list(fn = fnNesterov1, gr = grNesterov1),
+	                rastrigin = list(fn = fnRastrigin, gr = grRastrigin),
+	                rosenbrock = list(fn = fnRosenbrock, gr = grRosenbrock),
+					shor = list(fn = fnShor, gr = grShor),
+			 stop("Argument 'fname' not the name of a known test function.")
+		     )
+	if (is.null(tfunc$gr)) {
+		cat("'fname': numerical gradient will be computed.\n")
+		tfunc$gr <- function(x) ns.grad(tfunc$fn, x)
+	}
+	return(tfunc)
+}
 
 
 #-- Rosenbrock's non-convex performance test function ------------------------
@@ -16,12 +35,21 @@ grRosenbrock <- function(x) {
     g <- rep(NA, n)
     g[1] <- 2*(x[1] - 1) + 400*x[1] * (x[1]^2 - x[2])
     if (n > 2) {
-      for (i in 2:(n-1)) {
-        g[i] <- -2*(1-x[i]) + 400*x[i]*(x[i]^2-x[i+1]) + 200*(x[i]-x[i-1]^2)
-      }
+        ii <- 2:(n-1)
+        g[ii] <- 2*(x[ii]-1) + 400*x[ii]*(x[ii]^2-x[ii+1]) + 200*(x[ii]-x[ii-1]^2) 
     }
     g[n] <- 200*(x[n] - x[n-1]^2)
     g
+}
+
+#-- Rastrigin's function -----------------------------------------------------
+fnRastrigin <- function(x) {
+    n <- length(x)
+    10*n + sum(x^2 - 10*cos(2*pi*x))
+}
+
+grRastrigin <- function(x) {
+    2 * x + 20 * pi * sin(2 * pi * x)
 }
 
 #-- Nesterov's smooth Chebyshev-Rosenbrock function --------------------------
@@ -46,21 +74,32 @@ grNesterov <- function(x) {
     g
 }
 
-#-- Rastrigin's function -----------------------------------------------------
-fnRastrigin <- function(x) {
+#-- Nesterov's non-smooth Chebyshev-Rosenbrock functions ---------------------
+fnNesterov1 <- function(x) {
     n <- length(x)
-    10*n + sum(x^2 - 10*cos(2*pi*x))
+    f <- (1 - x[1])^2 / 4
+    for (i in 1:(n-1)) {
+        f <- f + abs(1 + x[i+1] - 2*x[i]^2)
+    }
+    f
+}
+grNesterov1 <- function(x) {
+    n <- length(x)
+    g <- rep(NA, n)
+    g[1] <- -(1 - x[1]) / 2
+    for (i in 1:(n-1)) {
+        r <- sign(1 + x[i+1] - 2*x[i]^2)
+        g[i+1] <- g[i+1] + r
+        g[i] <- g[i] - 4*x[i]*r
+    }
+    g
 }
 
-grRastrigin <- function(x) {
-    2 * x + 20 * pi * sin(2 * pi * x)
-}
-
-#-- Hald's function ----------------------------------------------------------
+#-- Non-smooth test function of Hald and Madsen ------------------------------
 # xmin = (0.99987763,  0.25358844, -0.74660757,  0.24520150, -0.03749029 )
 # fmin = 0.0001223713
 initHald <- function(x) {
-    # if (length(x) != 5) stop("Parameter vector for Hald must have length 5.")
+    stopifnot(is.numeric(x), length(x) == 5)
     i <- 1:21
     t <- -1 + (i - 1)/10
     (x[1] + x[2] * t) / ( 1 + x[3]*t + x[4]*t^2 + x[5]*t^3 ) - exp(t)
@@ -86,3 +125,46 @@ grHald <- function(x) {
     g
 }
 
+#-- Shor's piecewise quadratic function --------------------------------------
+# starting value c(1,1,1,1,1) in [0, 2]^5
+# xmin = (1.1243510, 0.9794616, 1.4777077, 0.9202335, 1.1242916)
+# fmin = 22.6001621
+fnShor <- function(x) {
+    stopifnot(is.numeric(x), length(x) == 5)
+    x <- as.matrix(c(x))
+    A <- matrix(
+    c(0,  2,  1,  1,  3,  0,  1,  1,  0,  1,
+      0,  1,  2,  4,  2,  2,  1,  0,  0,  1,
+      0,  1,  1,  1,  1,  1,  1,  1,  2,  2,
+      0,  1,  1,  2,  0,  0,  1,  2,  1,  0,
+      0,  3,  2,  2,  1,  1,  1,  1,  0,  0),
+    5, 10, byrow = TRUE)
+    b <- as.matrix(c(1, 5, 10, 2, 4, 3, 1.7, 2.5, 6, 4.5))
+    f <- 0
+    for (i in 1:10) {
+        d <- b[i] * sum((x - A[, i])^2)         # d[i] <- ...
+        if (d > f) f <- d                       # f <- max(d)
+    }
+    f
+}
+grShor <- function(x) {
+    stopifnot(is.numeric(x), length(x) == 5)
+    x <- as.matrix(c(x))
+    A <- matrix(
+    c(0,  2,  1,  1,  3,  0,  1,  1,  0,  1,
+      0,  1,  2,  4,  2,  2,  1,  0,  0,  1,
+      0,  1,  1,  1,  1,  1,  1,  1,  2,  2,
+      0,  1,  1,  2,  0,  0,  1,  2,  1,  0,
+      0,  3,  2,  2,  1,  1,  1,  1,  0,  0),
+    5, 10, byrow = TRUE)
+    b <- as.matrix(c(1, 5, 10, 2, 4, 3, 1.7, 2.5, 6, 4.5))
+    f <- 0
+    for (i in 1:10) {
+        d <- b[i] * sum((x - A[, i])^2)
+        if (d > f) {
+            f <- d
+            k <- i
+        }
+    }
+    c(2 * b[k] * (x - A[, k]))
+}
