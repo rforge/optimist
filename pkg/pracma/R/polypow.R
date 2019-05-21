8f892dbeba32897c1e7104b05b198c8d58426f0e
hwborchers <hwborchers@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-05-21 12:40:02 +0000

    Polynomial changes 2
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/optimist@475 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pracma/R/polypow.R
+++ pkg/pracma/R/polypow.R
@@ -19,38 +19,6 @@ polypow <- function(p, n){
 }
 
 
-polygcf <- function(p, q, tol=1e-12) {
-    if ( !is.vector(p, mode="numeric") && !is.vector(p, mode="complex") )
-        stop("Arguments 'p' must be a real or complex vector.")
-    if ( !is.vector(q, mode="numeric") && !is.vector(q, mode="complex") )
-        stop("Arguments 'q' must be a real or complex vector.")
-
-    np <- Norm(p)
-    pd <- polydiv(p,q)
-    a <- pd$d; r0 <- pd$r
-    if (Norm(r0) > np*tol) {
-        pd <- polydiv(q,r0)
-        a <- pd$d; r1 <- pd$r
-        if (Norm(r1) > np*tol) {
-            rn <- 1
-            while (Norm(rn) > np*tol) {
-                pd <- polydiv(r0,r1)
-                a <- pd$d; rn <- pd$r
-                r0 <- r1
-                r1 <- rn
-            }
-            g <- r0
-        } else {
-            g <- r0
-        }
-    } else {
-       g <- q
-    }
-    # g <- g / g[1]
-    return(g)
-}
-
-
 polytrans <- function(p, q){
     if ( (!is.vector(p, mode="numeric") && !is.vector(p, mode="complex")) ||
          (!is.vector(q, mode="numeric") && !is.vector(q, mode="complex")) )
